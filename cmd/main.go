5a40317c74365bfff237cbdf91bfd03030b24065
cortez1488 <mlm.ui0@mail.ru>
2022-04-27 20:27:01 +0500

    admin pannel

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
@@ -9,7 +10,9 @@ import (
 	"social-media-holding-test-task/internal/bot"
 	"social-media-holding-test-task/internal/core/admin"
 	"social-media-holding-test-task/internal/core/ip"
+	"social-media-holding-test-task/internal/core/user_info"
 	db2 "social-media-holding-test-task/internal/db"
+	"social-media-holding-test-task/internal/handler/rest"
 )
 
 func main() {
@@ -29,8 +32,23 @@ func main() {
 	admStorage := db2.NewAdminStorage(db)
 	admService := admin.NewAdminService(admStorage)
 
+	userInfoRepo := db2.NewUserInfoStorage(db)
+	userInfoService := user_info.NewUserInfoService(userInfoRepo, ipRepo)
+
+	handler := rest.NewHandler(userInfoService)
+	server := handler.InitRoutes()
+
 	telegramBot := bot.NewBot(botAPI, ipService, admService)
+
+	go runServer(server)
 	telegramBot.Start()
+}
+
+func runServer(server *gin.Engine) {
+	err := server.Run()
+	if err != nil {
+		log.Fatal("server: " + err.Error())
+	}
 }
 
 func getPostgresDBConnectString() string {
--- internal/handler/rest/main_handler.go
+++ internal/handler/rest/main_handler.go
@@ -1,4 +1,25 @@
 package rest
 
-type Hanlder struct {
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	userInfoService UserInfoService
+}
+
+func NewHandler(userInfoService UserInfoService) *Handler {
+	return &Handler{userInfoService: userInfoService}
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.Default()
+	api := router.Group("/api")
+	{
+		api.GET("/get_users", h.getUsers)
+		api.GET("/get_user", h.getUser)
+		api.GET("/get_history_by_tg", h.getHistoryByTgID)
+		api.DELETE("/delete_ip", h.deleteIp)
+	}
+	return router
 }
