31fd5d03b822cfa44ddfa6d2d2c63cf21d123ed2
cortez1488 <mlm.ui0@mail.ru>
2022-04-29 08:03:59 +0500

    errors

--- internal/handler/rest/user_info.go
+++ internal/handler/rest/user_info.go
@@ -4,12 +4,14 @@ import (
 	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
+	"net/http"
 	"strconv"
 )
 
 func (h *Handler) getUsers(c *gin.Context) {
 	result, err := h.userInfoService.GetUsers()
 	if err != nil {
+		c.JSON(http.StatusInternalServerError, "server error")
 		logrus.Fatal(errors.New("h.userInfoService.GetUsers: " + err.Error()))
 	}
 	c.JSON(200, result)
@@ -18,10 +20,12 @@ func (h *Handler) getUsers(c *gin.Context) {
 func (h *Handler) getUser(c *gin.Context) {
 	id, err := strconv.Atoi(c.Query("id"))
 	if err != nil {
+		c.JSON(http.StatusInternalServerError, "server error")
 		logrus.Fatal(errors.New("parse param" + err.Error()))
 	}
 	result, err := h.userInfoService.GetUser(id)
 	if err != nil {
+		c.JSON(http.StatusInternalServerError, "server error")
 		logrus.Fatal(errors.New("h.userInfoService.GetUser: " + err.Error()))
 	}
 	c.JSON(200, result)
@@ -30,6 +34,7 @@ func (h *Handler) getUser(c *gin.Context) {
 func (h *Handler) getHistoryByTgID(c *gin.Context) {
 	chatID, err := strconv.ParseInt(c.Query("chatid"), 10, 64)
 	if err != nil {
+		c.JSON(http.StatusInternalServerError, "server error")
 		logrus.Fatal(errors.New("parse param" + err.Error()))
 	}
 	history, err := h.userInfoService.GetHistoryByTgID(chatID)
@@ -40,6 +45,7 @@ func (h *Handler) deleteIp(c *gin.Context) {
 	ip := c.Query("ip")
 	err := h.userInfoService.DeleteIp(ip)
 	if err != nil {
+		c.JSON(http.StatusInternalServerError, "server error")
 		logrus.Fatal("h.userInfoService.DeleteIp: " + err.Error())
 	}
 	c.JSON(200, "deleted")
